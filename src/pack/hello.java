053b001560d78975f6bcca9c15a6650b49de7da2
ipsbaheti@gmail.com <ipsbaheti@gmail.com>
2022-04-26 11:48:47 -0700

    this is 2nd person commit

---
+++ src/pack/hello.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi i am making changes in project");
+	}
+
+}
