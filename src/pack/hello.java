9eccc04d2f6758fa02b94c48dd5197159dca391c
ipsbaheti@gmail.com <ipsbaheti@gmail.com>
2022-04-26 11:57:25 -0700

    orginal person commited 2nd time

--- src/pack/hello.java
+++ src/pack/hello.java
@@ -5,6 +5,9 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi i am making changes in project");
+		System.out.println("hi i am making changes in project1");
+		System.out.println("hi i am making changes in project2");
+		System.out.println("hi i am making changes in project3");
 	}
 
 }
